d3cbb3e4cdaed3a9e6dd8f358a287a5a61f4cad4
Jiwoo12345 <yahootomb370@gmail.com>
2024-12-10 12:37:20 +0100

    Replace regular CSS with Tailwind CSS

--- trailer.php
+++ trailer.php
@@ -4,32 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trailer</title>
-    <link rel="stylesheet" href="trailer.css">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-    <main>
-        <section class="container">
-            <div class="title-container">
-                <p class="title">Bluey</p>
+    <main class="bg-white">
+        <section class="flex items-center justify-between">
+            <div>
+                <p class="text-5xl">Bluey</p>
 
-                <div class="title-info">
+                <div class="flex gap-8 mb-4">
                     <p>TV series</p>
                     <p>2018</p>
                 </div>
             
-                <div class="image-container">
-                    <img class="image" src="./images/poster.jpg" alt="">
-                    <button class="watchlist-button">+</button>
+                <div class="ml-2 relative">
+                    <img class="rounded rounded-tl-none" src="./images/poster.jpg" alt="">
+                    <button class="absolute bg-black text-white text-xl top-0 p-2 opacity-50">+</button>
                 </div>
             </div>
 
             <div class="box-office-container">
-                <h2>Box office</h2>
-                <div class="box-grid">
-                    <h3>Budget</h3>
-                    <h3>Gross US & Canada</h3>
-                    <h3>Opening weekend US & Canada</h3>
-                    <h3>Gross worldwide</h3>
+                <h2 class="font-bold text-3xl">Box office</h2>
+                <div class="gap-4 grid grid-cols-2">
+                    <h3 class="font-bold">Budget</h3>
+                    <h3 class="font-bold">Gross US & Canada</h3>
+                    <h3 class="font-bold">Opening weekend US & Canada</h3>
+                    <h3 class="font-bold">Gross worldwide</h3>
                 </div>
             </div>
         </section>
