9fd9fdeea6f3848efaff457465d85c5a4b2fd1df
Jiwoo12345 <yahootomb370@gmail.com>
2024-12-10 13:39:45 +0100

    Replace the script tag and style the texts with Tailwind CSS

--- trailer.php
+++ trailer.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trailer</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <main class="bg-white">
-        <section class="flex items-center justify-between">
+        <section class="flex items-center justify-between ml-2">
             <div>
-                <p class="text-5xl">Bluey</p>
+                <p class="text-4xl">Bluey</p>
 
-                <div class="flex gap-8 mb-4">
+                <div class="flex gap-4 mb-4">
                     <p>TV series</p>
                     <p>2018</p>
                 </div>
             
-                <div class="ml-2 relative">
+                <div class="relative">
                     <img class="rounded rounded-tl-none" src="./images/poster.jpg" alt="">
                     <button class="absolute bg-black text-white text-xl top-0 p-2 opacity-50">+</button>
                 </div>
@@ -26,17 +26,29 @@
             <div class="box-office-container">
                 <h2 class="font-bold text-3xl">Box office</h2>
                 <div class="gap-4 grid grid-cols-2">
-                    <h3 class="font-bold">Budget</h3>
-                    <h3 class="font-bold">Gross US & Canada</h3>
-                    <h3 class="font-bold">Opening weekend US & Canada</h3>
-                    <h3 class="font-bold">Gross worldwide</h3>
+                    <div>
+                        <h3 class="font-bold">Budget</h3>
+                        <p>$50-65 million</p>
+                    </div>
+                    <div>
+                        <h3 class="font-bold">Gross US & Canada</h3>
+                        <p>$105.2 million</p>
+                    </div>
+                    <div>
+                        <h3 class="font-bold">Opening weekend US & Canada</h3>
+                        <p>$22.1 million</p>
+                    </div>
+                    <div>
+                        <h3 class="font-bold">Gross worldwide</h3>
+                        <p>$296.1 million</p>
+                    </div>
                 </div>
             </div>
         </section>
 
-        <section class="story-container">
-            <h3>Storyline</h3>
-            <p>
+        <section class="flex flex-col items-center">
+            <h3 class="font-bold mb-6 text-3xl">Storyline</h3>
+            <p class="w-3/5">
                 Bluey follows the adventures of a lovable and inexhaustible
                 six-year-old Blue Heeler puppy who lives with her dad, mum and
                 four-year-old little sister, Bingo. In every episode, Bluey
