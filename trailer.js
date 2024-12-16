757b3378d6f9131bf36e7df404931426c38c0307
Jiwoo12345 <yahootomb370@gmail.com>
2024-12-16 13:44:02 +0100

    Move the HTML code to JavaScript

--- trailer.js
+++ trailer.js
@@ -1,7 +1,8 @@
 const apiKey = "74595494";
+const container = document.querySelector("#container")
 
 function searchMovie() {
-    const title = "Bluey";
+    const title = "Red One";
     const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
     console.log(url);
 
@@ -9,11 +10,61 @@ function searchMovie() {
     .then(response => response.json())
     .then(data => {
         if (data.Response === "True") {
-            document.querySelector("#title").innerText = `${data.Title}`;
-            document.querySelector("#type").innerText = 'TV ' + `${data.Type}`
-            document.querySelector("#year").innerText = `${data.Year}`;
-            document.querySelector("#plot").innerText = `${data.Plot}`;
-            document.querySelector("#poster").src = data.Poster;
+            const text = `
+                <section>
+                <div>
+                    <div>
+                        <p id="title" class="text-4xl">${data.Title}</p>
+
+                        <div class="flex gap-4">
+                            <p id="type">${data.Type}</p>
+                            <p id="year">${data.Year}</p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="flex">
+                    <div class="relative">
+                        <img src="${data.Poster}" id="poster" class="rounded-xl rounded-tl-none h-auto w-52">
+                        <button class="absolute bg-black text-white text-xl top-0 p-2 opacity-50">+</button>
+                    </div>
+                    <div class="flex items-center">
+                            <iframe class="max-w-full" width="600" height="280" src="https://www.youtube.com/embed/ZrR9ML0sxEE" title="" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen></iframe>
+                        </div>
+
+                    <div>
+                    <h2 class="font-bold text-3xl">Box office</h2>
+                    <div class="gap-4 grid grid-cols-2">
+                        <div>
+                            <h3 class="font-bold">Budget</h3>
+                            <p>$50-65 million</p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold">Gross US & Canada</h3>
+                            <p>$105.2 million</p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold">Opening weekend US & Canada</h3>
+                            <p>$22.1 million</p>
+                        </div>
+                        <div>
+                            <h3 class="font-bold">Gross worldwide</h3>
+                            <p>$296.1 million</p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+            
+            <section>
+                <div>
+                    <p id="plot" class="w-5/6">${data.Plot}</p>
+                </div>
+            </section>
+            `
+
+            container.innerHTML = text;
         } else {
             alert('Cannot load movie from api');
         }
--- trailer.php
+++ trailer.php
@@ -8,56 +8,7 @@
 </head>
 <body>
     <main class="bg-white">
-        <div class="flex flex-col items-center justify-center">
-        <section class="">
-            <div>
-                <p id="title" class="text-4xl"></p>
-
-                <div class="flex gap-4">
-                    <p id="type"></p>
-                    <p id="year"></p>
-                </div>
-            </div>
-
-            <div class="flex">
-                <div class="relative">
-                    <img id="poster" class="rounded-xl rounded-tl-none h-auto w-52">
-                    <button class="absolute bg-black text-white text-xl top-0 p-2 opacity-50">+</button>
-                </div>
-                <div class="flex items-center">
-                    <iframe class="max-w-full" width="600" height="280" src="https://www.youtube.com/embed/ZrR9ML0sxEE" title="" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen></iframe>
-                </div>
-
-                <div>
-                <h2 class="font-bold text-3xl">Box office</h2>
-                <div class="gap-4 grid grid-cols-2">
-                    <div>
-                        <h3 class="font-bold">Budget</h3>
-                        <p>$50-65 million</p>
-                    </div>
-                    <div>
-                        <h3 class="font-bold">Gross US & Canada</h3>
-                        <p>$105.2 million</p>
-                    </div>
-                    <div>
-                        <h3 class="font-bold">Opening weekend US & Canada</h3>
-                        <p>$22.1 million</p>
-                    </div>
-                    <div>
-                        <h3 class="font-bold">Gross worldwide</h3>
-                        <p>$296.1 million</p>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </section>
-        
-        <section>
-            <div>
-                <p id="plot" class="w-5/6"></p>
-            </div>
-        </section>
-        </div>
+        <div id="container" class="flex flex-col items-center justify-center gap-3"></div>
     </main>
     <script src="trailer.js"></script>
 </body>
