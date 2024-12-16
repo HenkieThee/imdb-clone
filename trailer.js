5bddb71b60c0efeb4f7df60fe8c64014ed326e99
Jiwoo12345 <yahootomb370@gmail.com>
2024-12-16 14:37:42 +0100

    Remake the poster section
    The trailer will be added later

--- trailer.js
+++ trailer.js
@@ -11,26 +11,31 @@ function searchMovie() {
     .then(data => {
         if (data.Response === "True") {
             const text = `
-            <div class="bg-stone-900">
-                <section class="text-white">
-                    <div class="flex items-center justify-around p-6">
-                        <div class="">
-                            <p id="title" class="text-4xl">${data.Title}</p>
-                            <div class="flex gap-3 text-zinc-400">
-                                <p>${data.Year}</p>
-                                <p>${data.Rated}</p>
-                                <p>${data.Runtime}</p>
-                            </div>
-                        </div>
-                        <div class="flex flex-col items-center">
-                            <p>IMDb RATING</p>
-                            <p class="cursor-pointer text-gray-300 text-lg">
-                                <span class="text-xl font-bold text-white">${data.imdbRating}</span>/10
-                            </p>
+            <section class="bg-stone-900 flex flex-col text-white">
+                <div class="flex items-center justify-around">
+                    <div class="">
+                        <p id="title" class="text-4xl">${data.Title}</p>
+                        <div class="flex gap-3 text-zinc-400">
+                            <p>${data.Year}</p>
+                            <p>${data.Rated}</p>
+                            <p>${data.Runtime}</p>
                         </div>
                     </div>
-                </section>
-            </div>
+                    <div>
+                        <p>IMDb RATING</p>
+                        <p class="cursor-pointer text-gray-300 text-lg">
+                            <span class="text-xl font-bold text-white">${data.imdbRating}</span>/10
+                        </p>
+                    </div>
+                </div>
+            
+                <div class="flex justify-center">
+                    <div class="relative">
+                        <img src="${data.Poster}" id="poster" class="rounded-xl rounded-tl-none w-56">
+                        <button class="absolute bg-black text-white text-5xl top-0 opacity-50">+</button>
+                    </div>
+                </div>
+            </section>
             `
 
             container.innerHTML = text;
