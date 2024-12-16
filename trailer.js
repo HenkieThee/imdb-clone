1eb5041ce92e83c677d57988f7ec2b1a01da74dc
Jiwoo12345 <yahootomb370@gmail.com>
2024-12-16 14:17:56 +0100

    Remake the title section of the trailer page

--- trailer.js
+++ trailer.js
@@ -3,7 +3,7 @@ const container = document.querySelector("#container")
 
 function searchMovie() {
     const title = "Red One";
-    const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
+    const url = `http://www.omdbapi.com/?t="Red-One"&apikey=74595494`;
     console.log(url);
 
     fetch(url)
@@ -11,57 +11,26 @@ function searchMovie() {
     .then(data => {
         if (data.Response === "True") {
             const text = `
-                <section>
-                <div>
-                    <div>
-                        <p id="title" class="text-4xl">${data.Title}</p>
-
-                        <div class="flex gap-4">
-                            <p id="type">${data.Type}</p>
-                            <p id="year">${data.Year}</p>
+            <div class="bg-stone-900">
+                <section class="text-white">
+                    <div class="flex items-center justify-around p-6">
+                        <div class="">
+                            <p id="title" class="text-4xl">${data.Title}</p>
+                            <div class="flex gap-3 text-zinc-400">
+                                <p>${data.Year}</p>
+                                <p>${data.Rated}</p>
+                                <p>${data.Runtime}</p>
+                            </div>
+                        </div>
+                        <div class="flex flex-col items-center">
+                            <p>IMDb RATING</p>
+                            <p class="cursor-pointer text-gray-300 text-lg">
+                                <span class="text-xl font-bold text-white">${data.imdbRating}</span>/10
+                            </p>
                         </div>
                     </div>
-                    
-                </div>
-
-                <div class="flex">
-                    <div class="relative">
-                        <img src="${data.Poster}" id="poster" class="rounded-xl rounded-tl-none h-auto w-52">
-                        <button class="absolute bg-black text-white text-xl top-0 p-2 opacity-50">+</button>
-                    </div>
-                    <div class="flex items-center">
-                            <iframe class="max-w-full" width="600" height="280" src="https://www.youtube.com/embed/ZrR9ML0sxEE" title="" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen></iframe>
-                        </div>
-
-                    <div>
-                    <h2 class="font-bold text-3xl">Box office</h2>
-                    <div class="gap-4 grid grid-cols-2">
-                        <div>
-                            <h3 class="font-bold">Budget</h3>
-                            <p>$50-65 million</p>
-                        </div>
-                        <div>
-                            <h3 class="font-bold">Gross US & Canada</h3>
-                            <p>$105.2 million</p>
-                        </div>
-                        <div>
-                            <h3 class="font-bold">Opening weekend US & Canada</h3>
-                            <p>$22.1 million</p>
-                        </div>
-                        <div>
-                            <h3 class="font-bold">Gross worldwide</h3>
-                            <p>$296.1 million</p>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </section>
-            
-            <section>
-                <div>
-                    <p id="plot" class="w-5/6">${data.Plot}</p>
-                </div>
-            </section>
+                </section>
+            </div>
             `
 
             container.innerHTML = text;
--- trailer.php
+++ trailer.php
@@ -7,9 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <main class="bg-white">
-        <div id="container" class="flex flex-col items-center justify-center gap-3"></div>
-    </main>
+    <main id="container"></main>
     <script src="trailer.js"></script>
 </body>
 </html>
