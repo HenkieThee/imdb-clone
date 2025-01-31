713f19c13ad869264e38e3d63a456fb50e326a47
Jiwoo12345 <yahootomb370@gmail.com>
2025-01-31 11:33:29 +0100

    Make the styling for the trailer page better

--- trailer.php
+++ trailer.php
@@ -1,18 +1,110 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trailer</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="trailer.css">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <style>
+       .bg-custom {
+           background-color: #121212;
+       }
+       .category-custom {
+            background-color: #1f1f1f;
+       }
+       .video-bg {
+           position: relative;
+           width: 100%;
+           height: 200px;
+           overflow: hidden;
+           border-radius: 0.5rem;
+       }
+       .video-bg video {
+           position: absolute;
+           top: 0;
+           left: 0;
+           width: 100%;
+           height: 100%;
+           object-fit: cover;
+       }
+       .card-content {
+           position: relative;
+           z-index: 1;
+           background: rgba(0, 0, 0, 0.5);
+           padding: 1rem;
+           border-radius: 0.5rem;
+       }
+   </style>
 </head>
 <body>
+<nav class="bg-custom text-white p-4">
+       <div class="container mx-auto flex justify-between items-center">
+           <a href="#" class="flex items-center">
+               <img src="images/logo.png" alt="Logo" class="h-8 mr-3">
+               <span class="text-lg font-bold">IMDB Clone</span>
+           </a>
+           <form class="w-full max-w-2xl mx-auto flex relative">
+               <button id="dropdown-button" data-dropdown-toggle="dropdown" class="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">
+                   All <i class="fas fa-chevron-down ml-2.5"></i>
+               </button>
+               <div id="dropdown" class="z-10 hidden bg-black text-[#f4c519] divide-y divide-gray-100 rounded-lg shadow-sm w-44 absolute left-20 mt-12">
+                   <ul class="py-2 text-sm" aria-labelledby="dropdown-button">
+                       <li>
+                           <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-700">
+                               <i class="fas fa-film mr-2"></i> Titles
+                           </button>
+                       </li>
+                       <li>
+                           <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-700">
+                               <i class="fas fa-tv mr-2"></i> TV Episodes
+                           </button>
+                       </li>
+                       <li>
+                           <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-700">
+                               <i class="fas fa-user mr-2"></i> Celebs
+                           </button>
+                       </li>
+                       <li>
+                           <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-700">
+                               <i class="fas fa-building mr-2"></i> Companies
+                           </button>
+                       </li>
+                   </ul>
+               </div>
+               <div class="relative w-full">
+                   <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-white rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search movies..." required />
+                   <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-gray-900 bg-white rounded-e-lg border border-gray-300 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100">
+                       <i class="fas fa-search"></i>
+                       <span class="sr-only">Search</span>
+                   </button>
+               </div>
+           </form>
+           <div class="flex items-center">
+               <a href="" class="mx-2 flex items-center">
+                   <i class="fas fa-bookmark mr-1"></i> Watchlist
+               </a>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <a href="account.php" class="mx-2 flex items-center">
+                        <i class="fas fa-user mr-1"></i> Account
+                    </a>
+                <?php else: ?>
+                    <a href="signin.php" class="mx-2 flex items-center">
+                        <i class="fas fa-sign-in-alt mr-1"></i> Sign In
+                    </a>
+                <?php endif; ?>
+   </nav>
+
     <main id="container" class="bg-stone-900 text-white">
         <?php
         $omdbApiKey = "74595494";
         $tmdbApiKey = "ad44c9ef1393dce98f4d2f0cfc319492";
-        $title = "Shrek";
+        $title = "Shrek 2";
         $omdbUrl = "http://www.omdbapi.com/?t=" . urlencode($title) . "&apikey=" . $omdbApiKey;
 
         function fetchFromAPI($url) {
@@ -45,10 +137,13 @@
 
             $votes = $omdbData['imdbVotes'];
             $numberedVotes = intval(str_replace(",", "", $votes));
-            $trailerEmbed = $trailer ? "<iframe class='rounded-xl' width='760' height='386' src='https://www.youtube.com/embed/{$trailer['key']}' frameborder='0' allowfullscreen></iframe>" : "<p>No trailer available.</p>";
+
+            $trailerEmbed = $trailer
+            ? "<iframe class='rounded-xl' width='760' height='386' src='https://www.youtube.com/embed/{$trailer['key']}' frameborder='0' allowfullscreen></iframe>"
+            : "<p>No trailer available.</p>";
 
             echo "
-            <section class='mx-auto pb-10 pt-20 w-5/6'>
+            <section class='mx-auto py-10 w-5/6'>
                 <div class='flex justify-between w-[90%]'>
                     <div>
                         <p id='title' class='text-5xl'>{$omdbData['Title']}</p>
@@ -73,20 +168,36 @@
                 </div>
                 <div class='flex items-center'>
                     <div>
-                        <div class='relative'>
-                            <img src='{$omdbData['Poster']}' id='poster' class='rounded-xl rounded-tl-none w-64'>
-                            <button class='absolute bg-black text-white text-5xl top-0 opacity-50'>+</button>
+                        <div>
+                            <img src='{$omdbData['Poster']}' id='poster' class='rounded-xl w-64'>
                         </div>
                     </div>
                     <div class='px-4'>
                         {$trailerEmbed}
                     </div>
-                    <div class='flex flex-col gap-2'>
+                </div>
+
+                <div class='flex justify-between pt-4 w-[90%]'>
+                    <div class='flex flex-col gap-4 w-4/6'>
+                        <div>
+                            <p>{$omdbData['Genre']}</p>
+                        </div>
+                        <div>
+                            <p>{$omdbData['Plot']}</p>
+                        </div>
+                        <div class='flex flex-col gap-4'>
+                            <p class='font-bold'>Director <span class='casting-info'>{$omdbData['Director']}</span></p>
+                            <p class='font-bold'>Writers <span class='casting-info'>{$omdbData['Writer']}</span></p>
+                            <p class='font-bold'>Stars <span class='casting-info'>{$omdbData['Actors']}</span></p>
+                        </div>
+                    </div>
+
+                    <div class='flex flex-col justify-center gap-2'>
                         <button class='bg-yellow-500 cursor-pointer flex flex-nowrap font-semibold rounded text-black text-sm p-1'>Add to Watchlist</button>
                         <div>
-                            <span class='flex gap-2'>
-                                Metascore:
-                                <p class='" . ($omdbData['Metascore'] <= 50 ? 'bg-red-400' : 'bg-green-400') . " px-1 rounded'>{$omdbData['Metascore']}</p>
+                            <span class='flex gap-1'>
+                                <p class='" . ($omdbData['Metascore'] <= 50 ? 'bg-red-400' : 'bg-green-400') . " font-bold px-1'>{$omdbData['Metascore']}</p>
+                                Metascore
                             </span>
                         </div>
                         <div>
@@ -98,20 +209,7 @@
                     </div>
                 </div>
             </section>
-            <div class='flex flex-col gap-2 ml-32 pb-10 w-3/6'>
-                <div>
-                    <p>{$omdbData['Genre']}</p>
-                </div>
-                <div>
-                    <p>{$omdbData['Plot']}</p>
-                </div>
-                <div>
-                    <p class='font-bold'>Director <span class='casting-info'>{$omdbData['Director']}</span></p>
-                    <p class='font-bold'>Writers <span class='casting-info'>{$omdbData['Writer']}</span></p>
-                    <p class='font-bold'>Stars <span class='casting-info'>{$omdbData['Actors']}</span></p>
-                </div>
-                <p></p>
-            </div>";
+            ";
         } else {
             echo "<p>Cannot load movie from OMDb API</p>";
         }
